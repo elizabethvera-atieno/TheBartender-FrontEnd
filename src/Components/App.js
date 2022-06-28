fdd5b4d78c0943f4557cba64df28dd2fd13aa3e8
elizabethvera-atieno <atienolizvera@gmail.com>
2022-06-28 12:23:13 +0300

    create home,display,makecocktail components and add routes to the app component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Navbar from './Navbar';
-
+import Home from './Home';
+import Makecocktail from './MakeCocktail';
+import Display from './Display';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <div className='app'>
+        <Switch>
+          <Route path={'/display'}>
+            <Display/>
+          </Route>
+          <Route path={'/makeyours'}>
+            <Makecocktail/>
+          </Route>
+          <Route exact path={'/'}>
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
     </>
   );
 }
