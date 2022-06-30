fb11b6e2969623695c852a3887250602118f9468
elizabethvera-atieno <atienolizvera@gmail.com>
2022-06-30 18:37:03 +0300

    remove user prop

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,12 +4,10 @@ import Cards from "./Cards";
 import { useEffect } from "react";
 import { useState } from "react";
 
-
 function Home(){
-
-  const [drinkData, setDrinkData] = useState([]);
-
-  useEffect(() => {
+const [drinkData, setDrinkData] = useState([]);
+  
+useEffect(() => {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=c`)
       .then((r) => r.json())
       .then((data) => setDrinkData(data.drinks));
